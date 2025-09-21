3e25f8a2ea01f696d95d5b3ff4c92b431c870510
Surya1-2-3 <140395855+Surya1-2-3@users.noreply.github.com>
2025-09-21 16:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function incrementBtn(){
+	var para_element=document.getElementById("counter");
+	var number=Number(para_element.innerHTML);
+	alert(number);
+	number=number+1
+	para_element.innerHTML=number
+}
